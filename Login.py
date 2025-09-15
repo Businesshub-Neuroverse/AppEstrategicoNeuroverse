16b3511ae0d50de4784ea9fadb75ebe3dea57858
jucimaras7-byte <jucimaras7@gmail.com>
2025-09-15 11:30:48 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,32 +1,38 @@
 import streamlit as st
 import DashPedagogico as dp
-import bcrypt
-from config import engine  # importa a engine pronta
-from sqlalchemy import text
-import unicodedata
-import hmac
-import hashlib
-import os
-import logging
-from typing import Optional, List, Union
-from sqlalchemy.exc import OperationalError
 
-# Pega os parâmetros da URL
-params = st.experimental_get_query_params()
+# Forçar tema claro definindo CSS customizado
+st.markdown(
+        """
+        <style>
+            /* Fundo da página */
+            .stApp {
+                background-color: #FFFFFF;
+                color: #000000;
+            }
+            /* Títulos */
+            h1, h2, h3, h4, h5, h6 {
+                color: #000000;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+)
 
-# Acessa o email_hash (se existir)
+# -----------------------
+# Captura parâmetros da URL
+# -----------------------
+params = st.query_params()
 email_hash = params.get("email_hash", [None])[0]
-
-# Acessa a página (se existir)
-pagina = params.get("page", [None])[0]
+pagina = params.get("page", [None])[0] or ""
 
 
-st.write("paramento email_hash", email_hash)
-
-st.write("paramento página", pagina)
-# Chama a função passando o email_hash
-#dashboardPedegogico(email_hash=email_hash)
-
-
-#dp.dashboardPedegogico(email_hash)
-
+if pagina == "dash_ped":
+    if not email_hash:
+        st.error("Parâmetro 'email_hash' não fornecido na URL!")
+        st.stop()
+    else:
+        dp.dashboardPedegogico(email_hash)
+else:
+    st.error("Parâmetro 'page' não fornecido na URL - Página não Encontrada!")
+    st.stop()
